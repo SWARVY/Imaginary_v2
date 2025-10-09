cfcdfb8a85885f0c15d7fe79240fabf5a9f7b1f6
SWARVY <swarvy0826@naver.com>
2025-10-09 15:04:10 +0900

    feat: 새로운 텍스트 애니메이션 컴포넌트 추가 - Shuffle 및 LineShadow 구현, 기존 Shuffle 컴포넌트 삭제

--- src/shared/ui/text-animation/shuffle.tsx
+++ src/shared/ui/text-animation/shuffle.tsx
@@ -382,7 +382,7 @@ const Shuffle: React.FC<ShuffleProps> = ({
     'inline-block whitespace-normal break-words will-change-transform uppercase text-[4rem] leading-none';
   const commonStyle: React.CSSProperties = {
     textAlign,
-    fontFamily: `'Press Start 2P', sans-serif`,
+    fontFamily: `'Montserrat', sans-serif`,
     ...style,
   };
 
