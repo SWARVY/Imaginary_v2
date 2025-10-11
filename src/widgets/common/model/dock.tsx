1f4e8779b57e2dcb5caff48d37992cca5ca694f8
SWARVY <swarvy0826@naver.com>
2025-10-11 23:31:35 +0900

    feat: Dock 컴포넌트에 className prop 추가 및 DockItem에 소유자 전용 속성 추가

--- src/widgets/common/model/dock.tsx
+++ src/widgets/common/model/dock.tsx
@@ -1,11 +1,12 @@
 import { FileRouteTypes } from '@/routeTree.gen';
-import { AtSign, Compass, Contact, Signature } from 'lucide-react';
+import { AtSign, Compass, Contact, PencilLine, Signature } from 'lucide-react';
 import { Icon } from 'reshaped';
 
 interface DockAttributes {
   icon: React.ReactNode;
   label: string;
   path: FileRouteTypes['fullPaths'];
+  ownerOnly?: boolean;
 }
 
 export const DOCK_ITEMS: DockAttributes[] = [
@@ -13,20 +14,30 @@ export const DOCK_ITEMS: DockAttributes[] = [
     icon: <Icon svg={Compass} />,
     label: 'Explore',
     path: '/',
+    ownerOnly: false,
   },
   {
     icon: <Icon svg={Signature} />,
     label: 'Posts',
     path: '/posts',
+    ownerOnly: false,
   },
   {
     icon: <Icon svg={Contact} />,
     label: 'About',
     path: '/about',
+    ownerOnly: false,
   },
   {
     icon: <Icon svg={AtSign} />,
     label: 'Contact',
     path: '/contact',
+    ownerOnly: false,
+  },
+  {
+    icon: <Icon svg={PencilLine} />,
+    label: 'Write',
+    path: '/posts/write',
+    ownerOnly: true,
   },
 ];
--- src/widgets/common/ui/navigator.tsx
+++ src/widgets/common/ui/navigator.tsx
@@ -1,27 +1,30 @@
 import { DOCK_ITEMS } from '../model/dock';
 import { AuthModal, useAuthState } from '@/features/auth';
 import { Dock, DockItem } from '@/shared/ui/dock';
-import { useMatches, useRouter } from '@tanstack/react-router';
+import { useMatchRoute, useRouter } from '@tanstack/react-router';
 import { Lock, Unlock } from 'lucide-react';
 import { Icon } from 'reshaped';
 
 export default function Navigator() {
   const router = useRouter();
-  const matches = useMatches();
+  const matchRoute = useMatchRoute();
 
-  const { isSignedIn } = useAuthState();
+  const { isSignedIn, isOwner } = useAuthState();
 
   return (
-    <Dock>
-      {DOCK_ITEMS.map(({ label, path, icon }) => (
-        <DockItem
-          key={label}
-          label={label}
-          icon={icon}
-          onClick={() => router.navigate({ to: path })}
-          isActive={matches.some((match) => match.id === path)}
-        />
-      ))}
+    <Dock className="sticky top-0">
+      {DOCK_ITEMS.map(
+        ({ label, path, icon, ownerOnly }) =>
+          (!ownerOnly || (ownerOnly && isOwner)) && (
+            <DockItem
+              key={label}
+              label={label}
+              icon={icon}
+              onClick={() => router.navigate({ to: path })}
+              isActive={!!matchRoute({ to: path })}
+            />
+          ),
+      )}
       <AuthModal asChild>
         <DockItem
           label={isSignedIn ? 'Sign Out' : 'Sign In'}
